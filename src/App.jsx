86f9cb3a5bd4390bb4d116f849399074d810cc11
Johan Söderlund <johan.soderlund96@gmail.com>
2024-03-22 00:08:05 +0100

    first commit with components

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import "./App.css";
+import Header from "./Header.jsx";
+import Footer from "./Footer.jsx";
+import MainContent from "./MainContent.jsx";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <MainContent />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Footer.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <div>
+      <footer>
+        <small>© 2024 z1 development. All rights reserved.</small>
+      </footer>
+    </div>
+  );
+}
+
+export default Footer;
