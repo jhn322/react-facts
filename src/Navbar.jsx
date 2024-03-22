8b4a6e6376c025bb572e5a2b2e52328725fbe4f8
Johan Söderlund <johan.soderlund96@gmail.com>
2024-03-22 04:54:41 +0100

    new attempt to fix logo

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,9 +1,10 @@
 import React from "react";
+import reactLogo from "./assets/react.png";
 
 const Navbar = () => {
   return (
     <nav className="nav">
-      <img className="nav-logo" src="./src/assets/react.png" alt="React logo" />
+      <img className="nav-logo" src={reactLogo} alt="React logo" />
       <h3 className="nav-logo-text">ReactFacts</h3>
       <a href="https://react.dev/" target="_blank" className="nav-title">
         Learn something new!
